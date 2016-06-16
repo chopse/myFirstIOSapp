a631ae674672a3ba5bac40176b4d79ff140b460e
Christian Pendleton <christian.pendleton@softhouse.se>
2016-06-16 23:16:06 +0200

    Label setting made possible in UI.

--- FoodTracker/FoodTracker/ViewController.swift
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +22,11 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: Actions
+    @IBAction func setDefaultLabelText(sender: AnyObject) {
+        mealNameLabel.text = "Default text"
     }
 
 
